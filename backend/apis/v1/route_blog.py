294a91e2fd8aa7608109cf880dfeaf090d461fcd
Salma <salmabarbara1108@gmail.com>
2025-08-27 15:48:06 +0100

    get a blog and get the  lists of blog

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter,status
-from sqlalchemy.orm import session
+from typing import List
+from fastapi import APIRouter,status,HTTPException
+from sqlalchemy.orm import Session
 from fastapi import Depends
 
 from db.session import get_db
 from schemas.blog import ShowBlog,CreateBlog
-from db.repository .blog import create_new_blog
+from db.repository .blog import create_new_blog,retreive_blog,list_blogs
 
 router=APIRouter()
 
 @router.post("/blogs",response_model=ShowBlog,status_code=status.HTTP_201_CREATED)
-async def create_blog(blog:CreateBlog,db:session =Depends(get_db)):
+async def create_blog(blog:CreateBlog,db:Session =Depends(get_db)):
     blog =create_new_blog(blog=blog,db = db, author_id =1)
     return blog 
+
+@router.get("/blogs/{id}" ,response_model=ShowBlog)
+def get_blog(id: int, db:Session = Depends(get_db)):
+    blog=retreive_blog(id=id,db=db)
+    if not blog:
+        raise HTTPException(detail=f"Blog with {id} does not exist",status_code=status.HTTP_404_NOT_FOUND)
+    return blog
+
+
+@router.get("/blogs",response_model=List[ShowBlog])
+def get_all_blogs(db:Session =Depends(get_db)):
+    blogs= list_blogs(db=db)
+    return blogs
--- backend/db/repository/blog.py
+++ backend/db/repository/blog.py
@@ -9,3 +9,11 @@ def create_new_blog(blog: CreateBlog, db: Session, author_id:int = 1):
     db.commit()
     db.refresh(blog)
     return blog
+
+def retreive_blog(id: int, db: Session):
+    blog=db.query(Blog).filter(Blog.id ==id).first()
+    return blog
+
+def list_blogs (db:Session):
+    blogs=db.query(Blog).filter(Blog.is_active).all()
+    return blogs
